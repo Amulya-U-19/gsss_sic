138b2495dbcd140d70de026a5041c14adccc616d
Amulya-U-19 <amulyau19@gmail.com>
2025-08-13 12:27:35 +0530

    programs are added and some assignents are also added

---
+++ day 3/range.py
@@ -0,0 +1,9 @@
+num = 10
+
+for i in range(num):
+    print(f'i = {i}')
+    i += 2
+
+
+
+    
--- tax_level4.py
+++
@@ -1,3 +0,0 @@
-import tax_level3 as  t3 , tax_level1 as t1
-
-net_annnual_salary=t1.gross_annual_salary
